a4a2c5d8a97b960762b941e0b8b0517f6a23fa78
Peter <lpf20001229@gmail.com>
2026-08-20 17:51:46 -0700

    Add project files.

---
+++ QBODataLibrary/ChangeConn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QBODataLibrary
+{
+    public static class ChangeConn
+    {
+        public static void ChangeConnStr(string conn)
+        {
+            Properties.Settings.Default.SQLConnectionString = conn;
+        }
+    }
+}
---
+++ QBOLibrary/QboConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace QBOLibrary
+{
+    public static class QboConfig
+    {
+        public const string MinorVersion = "75";
+
+        public const string SandboxBaseUrl = "https://sandbox-quickbooks.api.intuit.com";
+        public const string ProductionBaseUrl = "https://quickbooks.api.intuit.com";
+        public const string TokenUrl = "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer";
+        public const string AuthorizeUrl = "https://appcenter.intuit.com/connect/oauth2";
+        public const string Scope = "com.intuit.quickbooks.accounting";
+
+        public static string ClientId { get; set; }
+        public static string ClientSecret { get; set; }
+        public static string RedirectUri { get; set; }
+
+        // Mirrors LIMSUser.SignedinDatabase.IsDev
+        public static bool IsDev { get; set; }
+
+        public static string Environment => IsDev ? "SANDBOX" : "PRODUCTION";
+        public static string BaseUrl => IsDev ? SandboxBaseUrl : ProductionBaseUrl;
+    }
+}
