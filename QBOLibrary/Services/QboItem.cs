fa4d1f42949b24e127f7358671c7263293b7a419
Peter <lpf20001229@gmail.com>
2026-08-21 17:07:40 -0700

    New files + class change

--- QBOLibrary/Services/QboItem.cs
+++ QBOLibrary/Services/QboItem.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QBOLibrary.Models
+namespace QBOLibrary.Services
 {
-    public class QboTermModel
+    public class QboItem
     {
     }
 }
---
+++ QBOTestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QBOTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Matches the LIMS runtime pattern - connection set at startup, not in config
+            QBODataLibrary.ChangeConn.ChangeConnStr(
+                "Data Source=10.1.10.249;Initial Catalog=LW_DEV;User ID=mylims1;Password=groundPork3$");
+
+            QBOLibrary.QboConfig.IsDev = true;
+            QBOLibrary.QboConfig.ClientId = "";
+            QBOLibrary.QboConfig.ClientSecret = "";
+
+            Console.WriteLine($"Environment: {QBOLibrary.QboConfig.Environment}");
+            Console.WriteLine($"Base URL:    {QBOLibrary.QboConfig.BaseUrl}");
+            Console.ReadLine();
+        }
+    }
+}
